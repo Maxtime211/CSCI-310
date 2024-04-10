142930b8eb2a342b1a78eaf747867c04f3f07794
Maxtime211 <143458947+Maxtime211@users.noreply.github.com>
2024-04-10 15:08:57 -0400

    Add files via upload

---
+++ Advanced Programming Project 2/items.js
@@ -0,0 +1,54 @@
+var item1 = {
+  image: "https://media.sweetwater.com/images/items/120/SM57-medium.jpg",
+  url: "https://www.sweetwater.com/store/detail/SM57",
+  manufacturer: "Shure",
+  productName: "SM57 Cardioid Dynamic Instrument Microphone",
+  itemid: "SM57",
+  quantity: 1,
+  price: 99.0,
+  description: "Dynamic Microphone with Cardioid Pickup Pattern, 40Hz-15kHz " +
+    "Frequency Response, Low Impedance, Includes Stand Adapter, and Zippered Carrying " +
+    "Case",
+  available: 9
+};
+
+var item2 = {
+  image: "https://media.sweetwater.com/images/guitars/SGSTHCCH/227130360/227130360-body-small.jpg",
+  url: "https://www.sweetwater.com/store/detail/SGSTHCCH",
+  manufacturer: "Gibson",
+  productName: "SG Standard Electric Guitar - Heritage Cherry",
+  itemid: "SGSTHCCH",
+  quantity: 1,
+  price: 1799.0,
+  serial: 227130360,
+  description: "Solidbody Electric Guitar with Mahogany Body, Mahogany Neck, " +
+    "Rosewood Fingerboard, and 2 Humbucking Pickups - Heritage Cherry",
+  available: 1
+};
+
+var item3 = {
+  image: "https://media.sweetwater.com/images/items/120/SM58-medium.jpg",
+  url: "https://www.sweetwater.com/store/detail/SM58",
+  manufacturer: "Shure",
+  productName: "SM58 Cardioid Dynamic Vocal Microphone",
+  itemid: "SM58",
+  quantity: 3,
+  price: 99.0,
+  description: "Dynamic Vocal Microphone with Cardioid Pickup Pattern and 50Hz- " +
+    "15kHz Frequency Response, Includes Stand Adapter, and Zippered Carrying Case",
+  available: 6
+};
+
+var item4 = {
+  image: "https://media.sweetwater.com/images/items/120/R16-medium.jpg",
+  url: "https://www.sweetwater.com/store/detail/R16",
+  manufacturer: "Zoom",
+  productName: "R16 16-track SD Recorder / Interface / Controller",
+  itemid: "R16",
+  quantity: 1,
+  price: 399.99,
+  description: "16-track Portable SD Recorder, USB Audio Interface, and DAW " +
+    "Control Surface with 8 Microphone Inputs, Built-in Stereo Condenser Microphones, " +
+    "Built-in Effects, 1GB SD Card, and USB - Mac/PC",
+  available: 14
+};
